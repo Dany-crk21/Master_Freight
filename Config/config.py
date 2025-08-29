2a564d9fdf728963aa43e5db57be38b610f1ce5d
Daniel <danielandrada.ds@gmail.com>
2025-08-29 20:52:19 -0300

    config

---
+++ Config/config.py
@@ -0,0 +1,9 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+user = os.getenv("DB_USER")
+password = os.getenv("DB_PASSWORD")
+host = os.getenv("DB_HOST")
+database = os.getenv("DB_NAME")
