9758217d1123c795de22d9462bb38228ade1cf92
Daniel <danielandrada.ds@gmail.com>
2025-09-12 19:46:12 -0300

    add path libh to config

--- Config/config.py
+++ Config/config.py
@@ -1,9 +1,14 @@
 from dotenv import load_dotenv
 import os
+from pathlib import Path
 
 load_dotenv()
 
-user = os.getenv("DB_USER")
-password = os.getenv("DB_PASSWORD")
-host = os.getenv("DB_HOST")
-database = os.getenv("DB_NAME")
+user = os.getenv("MYSQL_USER")
+password = os.getenv("MYSQL_PASSWORD")
+host = os.getenv("MYSQL_HOST")
+database = os.getenv("MYSQL_NAME")
+port = os.getenv("MYSQL_PORT","3307")
+
+BASE_DIR = Path(__file__).resolve().parent
+DATABASE_CONNECTION_URI = f"mysql+pymysql://{user}:{password}@{host}:{port}/{database}"
