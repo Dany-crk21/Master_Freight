326eeee04d64780283985a3ab9aca890291d9ef2
Daniel <danielandrada.ds@gmail.com>
2025-09-18 23:35:54 -0300

    Rutas de registro e inicio de sesion user y admin

--- Routes/Users.py
+++ Routes/Users.py
@@ -39,9 +39,61 @@ def register():
     new_user = User(
         username=data['username'],
         email=data['email'],
-        password= hashed_pw,
+        password= hashed_password,
         role=data.get('role','user') # por defecto es 'user'
     )     
     db.session.add(new_user)
     db.session.commit()
     return jsonify({"message":"User created sucessfully"}), 201
+   
+    
+@auth_bp.route("/register", methods=["GET"])
+def register_page():
+    return render_template("register.html")
+
+
+@auth_bp.route("/login", methods=["POST"])
+def login():
+    data = request.get_json()
+    user = User.query.filter_by(email=data["email"]).first()
+    if not user or not check_password_hash(user.password, data["password"]):
+        return jsonify({"message": "invalid credentials"}), 401
+    
+    token = jwt.encode({
+        'id': user.id,
+        'exp': datetime.utcnow() + timedelta(hours=1)
+    }, app.config['SECRET_KEY'], algorithm="HS256")
+     
+    # Aqui agregamos username ademas de token y role
+    return jsonify({
+        "token": token,
+        'role': user.role,
+        'username': user.username
+        })
+    
+@auth_bp.route("/dashboard", methods=["GET"])
+def dashboard_page():
+    return render_template("dashboard.html")
+
+@auth_bp.route("/api/dashboard", methods=["GET"])
+@token_required()
+def dashboard_api(current_user):
+    return jsonify({
+        "username": current_user.name,
+        'role': current_user.role
+    })
+
+@auth_bp.route("/api/users", methods=["GET"])
+@token_required(role='admin') # solo admin puede acceder
+def list_users(current_user):
+    users = User.query.all()
+    users_data = [
+        {
+            "id": u.id,
+            "username": u.username,
+            "email": u.email,
+            "role": u.role
+        }for u in users
+    ]
+    return jsonify(users_data)
+    
