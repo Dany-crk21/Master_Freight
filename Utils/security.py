8fccae8c52427250d83e1f4dbddaa47e2753bcde
Daniel <danielandrada.ds@gmail.com>
2025-09-19 21:38:07 -0300

    se agrega el modulo Utils sha-256

---
+++ Utils/security.py
@@ -0,0 +1,17 @@
+import os
+import hashlib
+
+#sha-256
+
+def _sha256_hex(data: bytes) -> str:
+    return hashlib.sha256(data).hexdigest()
+
+def make_password_hash(password: str) -> tuple[str,str]:
+    salt = os.urandom(16) # 16 bytes
+    salt_hex = salt.hex()
+    hash_hex = _sha256_hex(salt + password.encode('utf-8'))
+    return hash_hex, salt_hex
+
+def check_password(password: str, hash_hex: str, salt_hex: str) -> bool:
+    return _sha256_hex((salt_hex + password). encode('utf-8')) == hash_hex
+    
