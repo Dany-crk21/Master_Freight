1b91de1dff28cbc2ff5c01c524134f4c005f7d78
Daniel <danielandrada.ds@gmail.com>
2025-09-27 20:30:36 -0300

    correción de errores de modulo config

--- Config/config.py
+++ Config/config.py
@@ -7,7 +7,7 @@ load_dotenv()
 user = os.getenv("MYSQL_USER")
 password = os.getenv("MYSQL_PASSWORD")
 host = os.getenv("MYSQL_HOST")
-database = os.getenv("MYSQL_NAME")
+database = os.getenv("MYSQL_DB")
 port = os.getenv("MYSQL_PORT","3307")
 
 BASE_DIR = Path(__file__).resolve().parent
