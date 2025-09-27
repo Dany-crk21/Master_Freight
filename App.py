b89203e02de8ca724a3f5ee6bc30ce2cc397f90c
Daniel <danielandrada.ds@gmail.com>
2025-09-27 20:23:17 -0300

    correcíon de App

--- App.py
+++ App.py
@@ -5,23 +5,20 @@ from Routes.Users import auth_bp
 from flask_migrate import Migrate
 
 app = Flask(__name__)
-
-
-
 app.config.from_object(config)
-
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
 migrate = Migrate(app,db)
 app.register_blueprint(auth_bp)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+from Routes.Users import auth_bp
 
-if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
+with app.app_context():
+    db.create_all()  
+
+if __name__ == "__main__":
     app.run(debug=True)
+    
     
     
