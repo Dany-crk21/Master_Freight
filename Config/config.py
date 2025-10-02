4bbd589063e245898a28308affebe75300cb671d
Daniel <danielandrada.ds@gmail.com>
2025-10-02 20:09:17 -0300

    Update configuracion BD

--- Config/config.py
+++ Config/config.py
@@ -4,8 +4,13 @@ from pathlib import Path
 
 load_dotenv()
 
-class Config:
-    SECRET_KEY = os.getenv("JWT_SECRET", "SUPERSECRET")
-    SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{os.getenv('MYSQL_USER')}:{os.getenv('MYSQL_PASSWORD')}@{os.getenv('MYSQL_HOST')}:{os.getenv('MYSQL_PORT', '3306')}/{os.getenv('MYSQL_DATABASE')}"
-    
-SQLALCHEMY_TRACK_MODIFICATIONS = False
+user = os.getenv("MYSQL_USER")
+password = os.getenv("MYSQL_PASSWORD")
+host = os.getenv("MYSQL_HOST")
+database = os.getenv("MYSQL_DB")
+port = os.getenv("MYSQL_PORT","3306")
+JWT_SECRET = os.getenv("JWT_SECRET")
+
+BASE_DIR = Path(__file__).resolve().parent
+DATABASE_CONNECTION_URI = f"mysql+pymysql://{user}:{password}@{host}:{port}/{database}"
+SECRET_KEY = os.getenv('JWT_SECRET')
