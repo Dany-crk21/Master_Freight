ea14debdd82d0cc012ec3c392fe36500dcdacb55
Brian <Brianjuandev@gmail.com>
2025-10-26 15:24:14 -0300

    Add changes

---
+++ Routes/Clientes.py
@@ -0,0 +1,37 @@
+from flask import Blueprint, request, jsonify
+from Models.SolicitudFlete import SolicitudFlete
+from Models.Users_models import User
+from Models.db import db
+from Routes.Users import token_required
+
+Clientes_bp = Blueprint('clientes', __name__)
+
+# Rutas para clientes
+@Clientes_bp.route('/cliente/solicitar', methods=['POST'])
+@token_required(role='cliente')
+def solicitar_flete(current_user):
+    data = request.get_json()
+    nueva_solicitud = SolicitudFlete(
+        origen=data['origen'],
+        destino=data['destino'],
+        detalle=data['detalle'],
+        cliente_id=current_user.id
+    )
+    db.session.add(nueva_solicitud)
+    db.session.commit()
+    return jsonify({'message':'solicitud creada exitosamente'}), 201
+
+# Ver solicitudes del cliente
+@Clientes_bp.route('/cliente/solicitudes', methods=['GET'])
+@token_required(role='cliente')
+def ver_solicitudes(current_user):
+    solicitudes = SolicitudFlete.query.filter_by(cliente_id=current_user.id).all()
+    result = [{
+        'id': s.id,
+        'fecha': s.fecha,
+        'estado': s.estado,
+        'origen': s.origen,
+        'destino': s.destino,
+        'detalle': s.detalle,    }for s in solicitudes]
+    return jsonify(result), 200
+                   
---
+++ Routes/Fleteros.py
@@ -0,0 +1,38 @@
+from flask import Blueprint, request, jsonify
+from Models.SolicitudFlete import SolicitudFlete
+from Models.db import db
+from Routes.Users import token_required
+
+
+Fleteros_bp = Blueprint('fleteros', __name__)
+
+# Ver solicitudes asignadas al fletero
+@Fleteros_bp.route('/fletero/<int:fletero_id>/solicitudes', methods=['GET'])
+@token_required(role='fletero')
+def ver_solicitudes(current_user):
+    solicitudes = SolicitudFlete.query.filter_by(estado='pendiente').all()
+    result = [{
+        'id': s.id,
+        'fecha': s.fecha.isoformat(),
+        'estado': s.estado,
+        'origen': s.origen,
+        'destino': s.destino,
+        'detalle': s.detalle,
+        'fecha': s.fecha,
+    }for s in solicitudes]
+    return jsonify(result), 200
+
+# Aceptar o Rechazar solicitud
+@Fleteros_bp.route('/fletero/responder', methods=['PUT'])
+@token_required(role='fletero')
+def responder_solicitud(current_user):
+    data = request.get_json()
+    solicitud = SolicitudFlete.query.get(data['solicitud_id'])
+    if not solicitud:
+        return jsonify({'error': 'Solicitud no encontrada'}), 404
+    
+    solicitud.estado = data['estado'] # 'aceptada' o 'rechazada'
+    solicitud.fletero_id = current_user.id
+    db.session.commit()
+    return jsonify({"message": f"solicitud{data['estado']} correctamente"}), 200
+
