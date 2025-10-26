a0ee593694eab9da3b00c7f107daae718b5dbe94
Daniel <danielandrada.ds@gmail.com>
2025-10-26 14:52:39 -0300

    ruta de Inicio y registro de clientes y Fleteros con sus respectivos Dasboard

--- Routes/Users.py
+++ Routes/Users.py
@@ -1,35 +1,12 @@
 from flask import Blueprint, request, jsonify, render_template
 from Models.Users_models import User
 from Models.db import db
-from Utils.security import make_password_hash, check_password
+from Utils.security import make_password_hash, check_password, token_required
 import jwt
 from flask import current_app as app
-from functools import wraps
 from datetime import datetime, timedelta
 
 auth_bp = Blueprint('auth',__name__)
-
-def token_required(role=None):
-    def decorator(f):
-        @wraps(f)
-        def decorated(*args, **kwargs):
-                token = request.headers.get('Authorization')
-                if not token:
-                    return jsonify({"message":"Token is missing!"}), 401
-                try:
-                    token = token.split()[1] # Bearer <token>
-                    data = jwt.decode(token, app.config['SECRET_KEY'], algorithms=["HS256"])
-                    current_user = User.query.get(data['id'])
-                    if not current_user:
-                        return jsonify({"message":"User not found"}), 404
-                    if role and current_user.role != role:
-                        return jsonify({"message":"Unauthorized access"}), 403
-                except Exception as e:
-                    return jsonify({"message":"Token is invalid!",'error':str(e)}), 401
-                return f(current_user, *args, **kwargs)
-        return decorated
-    return decorator
-
 
 @auth_bp.route('/register/cliente', methods=['POST'])
 def register_cliente():
