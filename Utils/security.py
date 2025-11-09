d4f4cb26fa1380d63186b66d7350657a4f3137d1
Daniel <danielandrada.ds@gmail.com>
2025-11-09 10:06:13 -0300

    movido de ToKen Required para proteger rutas

--- Utils/security.py
+++ Utils/security.py
@@ -1,8 +1,11 @@
 import os
 import hashlib
+import jwt
+from flask import request, jsonify, current_app as app
+from functools import wraps
+from Models.Users_models import User
 
 #sha-256
-
 def _sha256_hex(data: bytes) -> str:
     return hashlib.sha256(data).hexdigest()
 
@@ -16,3 +19,23 @@ def check_password(password: str, hash_hex: str, salt_hex: str) -> bool:
     salt = bytes.fromhex(salt_hex)
     return _sha256_hex(salt + password.encode('utf-8')) == hash_hex
     
+def token_required(role=None):
+    def decorator(f):
+        @wraps(f)
+        def decorated(*args, **kwargs):
+                token = request.headers.get('Authorization')
+                if not token:
+                    return jsonify({"message":"Token is missing!"}), 401
+                try:
+                    token = token.split()[1] # Bearer <token>
+                    data = jwt.decode(token, app.config['SECRET_KEY'], algorithms=["HS256"])
+                    current_user = User.query.get(data['id'])
+                    if not current_user:
+                        return jsonify({"message":"User not found"}), 404
+                    if role and current_user.role != role:
+                        return jsonify({"message":"Unauthorized access"}), 403
+                except Exception as e:
+                    return jsonify({"message":"Token is invalid!",'error':str(e)}), 401
+                return f(current_user, *args, **kwargs)
+        return decorated
+    return decorator
