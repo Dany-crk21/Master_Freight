a70c67db2ec7013f96f531423646cec60b1c66a2
Daniel <danielandrada.ds@gmail.com>
2025-11-10 20:19:42 -0300

    configuracion de env

--- Config/config.py
+++ Config/config.py
@@ -8,7 +8,7 @@ user = os.getenv("MYSQL_USER")
 password = os.getenv("MYSQL_PASSWORD")
 host = os.getenv("MYSQL_HOST")
 database = os.getenv("MYSQL_DB")
-port = os.getenv("MYSQL_PORT","3307")
+port = os.getenv("MYSQL_PORT",)
 JWT_SECRET = os.getenv("JWT_SECRET")
 
 BASE_DIR = Path(__file__).resolve().parent
